e5b70afe1df86fdae2e472a284c0ff01dac63b71
Suyatna <zuyatna@gmail.com>
2021-10-27 00:02:58 +0700

    chapter02 - shift operations

---
+++ chapter02/src/main/kotlin/Main.kt
@@ -0,0 +1,33 @@
+fun main() {
+
+    // This is a comment. It is not executed.
+
+    // This is also a comment.
+    // Over multiple lines.
+
+    /* This is also a comment.
+    *  Over many
+    *  Many...
+    *  Many Lines. */
+
+    // Printing Out
+//    println("Hello, kotlin apprentice reader!!")
+
+    // Arithmetic Operations
+    // Simple Operations
+//    println(2 + 6)
+//    println(10 - 2)
+//    println(2 * 4)
+//    println(24 / 3)
+
+    // Decimal Numbers
+//    println(22.0 / 7.0)
+
+    // The Reminder Operation
+    // Modulo
+//    println("%.0f".format(28.0 % 10.0))
+
+    // Shift Operations
+    println(1 shl 3)
+    println(32 shr 2)
+}
