82307a0ce2694b7e4cf08cc58cd6b040df2e1893
Suyatna <zuyatna@gmail.com>
2021-10-30 19:56:28 +0700

    chapter03 - type inference

---
+++ chapter03/src/main/kotlin/Main.kt
@@ -0,0 +1,28 @@
+fun main() {
+    // Type Conversion
+//    var integer: Int = 100
+//    var decimal: Double = 12.5
+//    // integer = decimal
+    /*
+    Type mismatch.
+    Required: Int
+    Found: Double
+     */
+
+//    integer = decimal.toInt()
+
+    // Operator With Mixed Types
+//    val hourlyRate: Double = 19.5
+//    val hoursWorked: Int = 10
+//    val totalCost: Double = hourlyRate * hoursWorked
+//    println(totalCost)
+
+    // Type Inference
+//    val typeInferredInt = 42
+//    val typeInferredDouble = 3.14159
+//    val actuallyDouble = 3.toDouble()
+
+    // Mini-Exercises
+
+
+}
