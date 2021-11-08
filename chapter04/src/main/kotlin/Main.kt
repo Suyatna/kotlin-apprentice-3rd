37dba23db7d531d27b791a35d2b5507a1143cf43
Suyatna <zuyatna@gmail.com>
2021-11-08 10:53:06 +0700

    chapter04 - control flow

---
+++ chapter04/src/main/kotlin/Main.kt
@@ -0,0 +1,15 @@
+fun main() {
+    // Boolean
+    val yes = true
+    val no = false
+
+    // Boolean Operators
+//    val doesOneEqualTwo = (1 == 2)
+//    val doesOneNotEqualTwo = (1 != 2)
+
+    // ! not-operator
+//    val alsoTrue = !(1 == 2)
+    // true
+
+
+}
