7b5fa5ca6eac284cca78287531f66348db6d7f5c
Suyatna <zuyatna@gmail.com>
2021-11-08 13:11:32 +0700

    chapter05 - repeat loops

--- chapter05/src/main/kotlin/Main.kt
+++ chapter05/src/main/kotlin/Main.kt
@@ -1,5 +1,35 @@
 fun main() {
     // Ranges
+    // Closed Range
     val closedRange = 0..5
     println(closedRange)
+
+    // Half-Open Range
+    val halfOpenRange = 0 until 5
+//    println(halfOpenRange)
+
+    // Decreasing Range
+    val decreasingRange = 5 downTo 0
+//    println(decreasingRange)
+
+    // For Loops
+    val count = 10
+
+    var sum = 0
+//    for (i in 1..count) {
+//        sum += i
+//        print("$sum ")
+//    }
+
+    // Repeat Loops
+    sum = 1
+    var lastSum = 0
+    repeat(10) {
+        val temp = sum
+        sum += lastSum
+        print("sum: $sum ")
+        lastSum = temp
+        print("lastSum: $lastSum ")
+        println()
+    }
 }
