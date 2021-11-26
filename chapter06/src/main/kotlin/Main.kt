261c65bd4245655cc17d8f0434a97850dc9dca2e
Suyatna <zuyatna@gmail.com>
2021-11-26 18:48:48 +0700

    chapter06 - function expression

--- chapter06/src/main/kotlin/Main.kt
+++ chapter06/src/main/kotlin/Main.kt
@@ -1,7 +1,17 @@
 fun main() {
     printMyName()
     printMultipleOfFive(10)
-    printMultipleOf(4, 2)
+
+    // parameter named arguments
+    printMultipleOf(multiplier = 4, andValue = 2)
+
+    println(multiply(2, 2))
+
+    val (product, quotient) = multiplyAndDivide(4, 2)
+    println("product: $product")
+    println("quotient: $quotient")
+
+    println("fun inferred: ${multiplyInferred(3, 3)}")
 }
 
 // function basic
@@ -14,6 +24,19 @@ fun printMultipleOfFive(value: Int) {
     println("$value * 5 = ${value * 5}")
 }
 
-fun printMultipleOf(multiplier: Int, andValue: Int) {
+// parameter default values
+fun printMultipleOf(multiplier: Int, andValue: Int = 1) {
     println("$multiplier * $andValue = ${multiplier * andValue}")
 }
+
+// return value
+fun multiply(number: Int, multiplier: Int): Int {
+    return number * multiplier
+}
+
+fun multiplyAndDivide(number: Int, factor: Int): Pair<Int, Int> {
+    return Pair(number * factor, number / factor)
+}
+
+// function expression
+fun multiplyInferred(number: Int, multiplier: Int) = number * multiplier
