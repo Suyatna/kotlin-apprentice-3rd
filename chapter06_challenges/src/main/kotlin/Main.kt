bd932ee9d8063021644ee840c33f6e2347d683be
Suyatna <zuyatna@gmail.com>
2021-12-04 16:33:54 +0700

    chapter06_challenges - 2. fibonacci

--- chapter06_challenges/src/main/kotlin/Main.kt
+++ chapter06_challenges/src/main/kotlin/Main.kt
@@ -2,7 +2,20 @@ fun main() {
     println("Hello World!")
     println()
 
-    println(isPrime(89))
+    println(isPrime(6))
+    println(isPrime(13))
+    println(isPrime(8893))
+    println()
+
+    println(fibonacci(1))
+    println(fibonacci(2))
+    println(fibonacci(3))
+    println(fibonacci(4))
+    println(fibonacci(5))
+    println(fibonacci(6))
+    println(fibonacci(7))
+    println(fibonacci(10))
+    println()
 }
 
 // 1. it's prime time
@@ -19,4 +32,22 @@ fun isNumberDivisible(number: Int, divisor: Int): Boolean {
 
 fun isPrime(number: Int): Boolean {
     return isNumberDivisible(number, 2)
+}
+
+// 2. recursive functions
+fun fibonacci(number: Int): Int {
+    var finalNumber = 0
+
+    for (i in 0..number) {
+        finalNumber = when (i) {
+            0 -> 0
+            1 -> 1
+            2 -> 1
+            else -> {
+                fibonacci(number-1) + fibonacci(number-2)
+            }
+        }
+    }
+
+    return finalNumber
 }
