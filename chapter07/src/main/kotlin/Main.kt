dafd7b4867f273cc81713aaff196b4c24131ff28
Suyatna <zuyatna@gmail.com>
2021-12-05 12:23:42 +0700

    chapter07 - Smart Cast, Mini-Exercises 2

---
+++ chapter07/src/main/kotlin/Main.kt
@@ -0,0 +1,60 @@
+fun main() {
+    // Mini-Exercises
+    // 1.
+    var myFavoriteSong: String? = null
+
+    // 2.
+    // val parsedInt = "10".toIntOrNull()
+    // nullable because Int?
+
+    // 3.
+    // val parsedInt = "dog".toIntOrNull()
+    // equal null
+
+    // Checking For Null
+    var result: Int? = 30
+    println(result)
+    println()
+    // println(result + 1) // will get error, which is not allowed on a nullable receiver
+
+    // Not-Null Assertion Operator
+    var authorName: String? = "Joe Howard"
+    var authorAge: Int? = 24
+
+    val ageAfterBirthday = authorAge!! + 1
+    println("After their next birthday, $authorName will be $ageAfterBirthday")
+    println()
+
+//    authorAge = null
+//    println("After two next birthday, $authorName will be ${authorAge!! + 2}")
+//    // Pointer Exception
+//    // Compiler Warning 'ALWAYS_NULL' Options
+
+    // Smart Cast
+//    var nonNullableAuthor: String
+//    var nullableAuthor: String?
+//
+//    if (authorName != null) {
+//        nonNullableAuthor = authorName
+//    } else {
+//        nullableAuthor = authorName
+//    }
+
+    // Mini-Exercises 2
+    // 1.
+    if (myFavoriteSong != null) {
+        println("I have a favorite song.")
+    } else {
+        println("I don't have a favorite song.")
+    }
+    println()
+
+    // 2.
+    if (myFavoriteSong != null) {
+        myFavoriteSong = null
+        println("Now I have a favorite song is $myFavoriteSong")
+    } else {
+        myFavoriteSong = "You're my world"
+        println("Now I have a favorite song is $myFavoriteSong")
+    }
+}
