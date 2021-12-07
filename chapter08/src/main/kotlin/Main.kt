674fb5573bb9dd4008741574b00b14b0ae131012
Suyatna <zuyatna@gmail.com>
2021-12-07 13:26:20 +0700

    chapter08 - creating arrays

--- chapter08/src/main/kotlin/Main.kt
+++ chapter08/src/main/kotlin/Main.kt
@@ -4,4 +4,26 @@ fun main(args: Array<String>) {
     // Try adding program arguments via Run/Debug configuration.
     // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
     println("Program arguments: ${args.joinToString()}")
+    println()
+
+    // Creating Arrays
+    // val evenNumber = arrayOf(2, 4, 6, 8)
+
+    // Array With All Of Its Values Set To A Default
+    // val fiveFive = Array(5, { 5 }) // 5 5 5 5 5
+
+    // Array Of Primitive Types
+    // arrayOf() -> Array<Int>, can correspond to array of primitive type.
+    // val oddNumbers = intArrayOf(1, 3, 5, 7)
+
+    // Other standard library functions include:
+    // floatArrayOf(), doubleArrayOf(), booleanArrayOf().
+    // Various functions: IntArray, FloatArray, DoubleArray, etc.
+
+    // Pass a number into the constructor for these type, for example:
+    // val zero = DoubleArray(4) // 0.0 0.0 0.0 0.0
+    // Convert between the boxed and primitive arrays using function
+    // val otherOddNumbers = arrayOf(1, 3, 5, 7).toIntArray()
+
+    // Arguments To main()
 }
