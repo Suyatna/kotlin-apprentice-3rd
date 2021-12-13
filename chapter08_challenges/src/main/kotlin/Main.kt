7067446147e23c2ad64c3631771663899be1e2f0
Suyatna <zuyatna@gmail.com>
2021-12-13 16:22:48 +0700

    chapter08_challenges - challenge 4

--- chapter08_challenges/src/main/kotlin/Main.kt
+++ chapter08_challenges/src/main/kotlin/Main.kt
@@ -5,7 +5,7 @@ fun main() {
     // 1. valid statements
     // val array1 = Array<Int>() // not valid -> what a specific size
     // val array2 = arrayOf() // not enough information to infer type variable T, what value inside variable T
-    val array3: Array<String> = arrayOf() // valid
+    // val array3: Array<String> = arrayOf() // valid
 
     val array4 = arrayOf(1, 2, 3)
 
@@ -13,7 +13,7 @@ fun main() {
     // println(array4[5]) // index 5 out of bounds for length 3
     array4[0] = 4 // valid
 
-    val array5 = arrayOf(1, 2, 3)
+    val array5 = arrayOf(1, 2, 3, 4, 5)
 
     array5[0] = array5[1] // valid
     // array5[0] = "six" // type mismatch
@@ -31,4 +31,33 @@ fun main() {
     val number = listOf(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
     val newNumber = removeOne(4, number)
     println(newNumber.joinToString())
+
+    // 3. write a function that removes all occurrences of a given integer from a list of integers.
+    fun remove(item: Int, list: List<Int>): List<Int> {
+        val mutableList = list.toMutableList()
+        while (item in mutableList) {
+            mutableList.remove(item)
+        }
+
+        return mutableList.toList()
+    }
+
+    val repeatedFours = listOf(1, 2, 3, 4, 5, 6, 7, 8, 9, 4)
+    val noFours = remove(4, repeatedFours)
+    println(noFours.joinToString())
+
+    // 4. write a function that does a similar thing, without reverse, and returns a new array with the element
+    // of the original array in reverse order.
+    fun reverse(array: Array<Int>): Array<Int> {
+        val newArray = Array(array.size) { 0 }
+        for (i in array.indices) {
+            newArray[i] = array[array.size - i - 1]
+        }
+
+        return newArray
+    }
+
+    val array6 = arrayOf(1, 2, 3, 4, 5, 6, 7, 8, 9)
+    val arrayReversed = reverse(array6)
+    println(arrayReversed.joinToString())
 }
