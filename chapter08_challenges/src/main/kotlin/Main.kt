707ca43e57e334ff4f78b3b6409d6fbaecf425d6
Suyatna <zuyatna@gmail.com>
2021-12-13 19:37:25 +0700

    chapter08_challenges - challenge 5

--- chapter08_challenges/src/main/kotlin/Main.kt
+++ chapter08_challenges/src/main/kotlin/Main.kt
@@ -1,3 +1,5 @@
+import java.util.Random
+
 fun main() {
     println("Hello World!")
     println()
@@ -60,4 +62,40 @@ fun main() {
     val array6 = arrayOf(1, 2, 3, 4, 5, 6, 7, 8, 9)
     val arrayReversed = reverse(array6)
     println(arrayReversed.joinToString())
+
+    // 5. the function below returns a random number between from (inclusive) and the to (exclusive).
+    // write a function that shuffle the elements of an array in random order
+    val random = Random()
+    fun rand(from: Int, to: Int): Int {
+        return random.nextInt(to - from) + from
+    }
+
+    fun randomized(array: Array<Int>): Array<Int> {
+        val newArray = Array(array.size) { 0 }
+        val usedIndices = mutableListOf<Int>()
+
+        for (i in array.indices) {
+            var randomIndex: Int
+            while (true) {
+                randomIndex = rand(0, array.size)
+                if (randomIndex !in usedIndices) {
+                    usedIndices.add(randomIndex)
+                    break
+                }
+            }
+            newArray[i] = array[randomIndex]
+        }
+
+        return newArray
+    }
+
+    val arrayOrdered = arrayOf(0, 1, 2, 3, 4, 5, 6, 7, 8, 9)
+    val arrayRandomized = randomized(arrayOrdered)
+    println(arrayRandomized.joinToString())
+    println()
+
+    println(arrayOrdered.size)
+    for (i in 0..50) {
+        print("${rand(0, arrayOrdered.size)} ")
+    }
 }
