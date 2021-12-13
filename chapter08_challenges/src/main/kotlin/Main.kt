2a6ced4eca70213d2053be13f75140054a75595a
Suyatna <zuyatna@gmail.com>
2021-12-13 14:13:40 +0700

    chapter08_challenges - challenge 2

--- chapter08_challenges/src/main/kotlin/Main.kt
+++ chapter08_challenges/src/main/kotlin/Main.kt
@@ -1,7 +1,34 @@
-fun main(args: Array<String>) {
+fun main() {
     println("Hello World!")
+    println()
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+    // 1. valid statements
+    // val array1 = Array<Int>() // not valid -> what a specific size
+    // val array2 = arrayOf() // not enough information to infer type variable T, what value inside variable T
+    val array3: Array<String> = arrayOf() // valid
+
+    val array4 = arrayOf(1, 2, 3)
+
+    println(array4[0]) // valid
+    // println(array4[5]) // index 5 out of bounds for length 3
+    array4[0] = 4 // valid
+
+    val array5 = arrayOf(1, 2, 3)
+
+    array5[0] = array5[1] // valid
+    // array5[0] = "six" // type mismatch
+    // array5 += 6 // val cannot reassign
+    // for item in array5 { println(item) } // should include ()
+
+    // 2. write a function that removes the first occurrence of a given integer from a list of integer.
+    fun removeOne(item: Int, list: List<Int>): List<Int> {
+        val mutableList = list.toMutableList()
+        mutableList.remove(item)
+
+        return mutableList.toList()
+    }
+
+    val number = listOf(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+    val newNumber = removeOne(4, number)
+    println(newNumber.joinToString())
 }
