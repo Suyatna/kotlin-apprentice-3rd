ae5098f677d9548c16748ca5325f1fb8aa323fea
Suyatna <zuyatna@gmail.com>
2021-12-16 20:54:35 +0700

    chapter09 - modifying mutable maps, adding pairs

--- chapter09/src/main/kotlin/Main.kt
+++ chapter09/src/main/kotlin/Main.kt
@@ -19,4 +19,16 @@ fun main() {
     // println(namesAndScores.get("Craig"))
     println(namesAndScores.isEmpty())
     println(namesAndScores.size)
+
+    // modifying mutable maps
+    // adding pairs
+    val bobData = mutableMapOf(
+        "name" to "Bob",
+        "profession" to "CardPlayer",
+        "country" to "USA"
+    )
+
+    bobData["state"] = "CA"
+    bobData["city"] = "San Francisco"
+    println(bobData)
 }
