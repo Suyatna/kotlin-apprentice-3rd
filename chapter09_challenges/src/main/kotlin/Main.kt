1f2fac1ab367fe493ba9693e21624cf6d4d9f7cb
Suyatna <zuyatna@gmail.com>
2021-12-22 13:17:48 +0700

    chapter09 - delete challenge, move to chapter09_challenge

--- chapter09_challenges/src/main/kotlin/Main.kt
+++ chapter09_challenges/src/main/kotlin/Main.kt
@@ -1,7 +1,22 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
+fun main() {
+    // challenges
+    // 1. valid statements?
+    // val map1: Map<Int to Int> = emptyMap() // type was wrong: Map<K, V>
+    // val map2 = emptyMap() // not enough information: Map<K, V>
+    val map3: Map<Int, Int> = emptyMap()
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+    val map4 = mapOf("One" to 1, "Two" to 2, "Three" to 3)
+    // map4[1] // type inference failed. the value of the type parameter K should be mentioned in input types (argument types, receiver type or expected type)
+    map4["one"]
+    // map4["Zero"] = 0 // no set method providing array access
+    // map4[0] = "Zero" // unresolved reference. none of the following candidates is applicable because of receiver type mismatch
+
+    val map5 = mutableMapOf(
+        "NY" to "New York",
+        "CA" to "California"
+    )
+
+    map5["NY"]
+    map5["WA"] = "Washington"
+    // map5["CA"] = null // null can not be a value of a non-null type TypeVariable(V)
 }
