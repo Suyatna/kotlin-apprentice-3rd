0c22147e55b64bd34f7fb8e630b7fbce594cc6c9
Suyatna <zuyatna@gmail.com>
2021-12-22 13:46:22 +0700

    chapter09_challenges - half-challenge was solved

--- chapter09_challenges/src/main/kotlin/Main.kt
+++ chapter09_challenges/src/main/kotlin/Main.kt
@@ -19,4 +19,36 @@ fun main() {
     map5["NY"]
     map5["WA"] = "Washington"
     // map5["CA"] = null // null can not be a value of a non-null type TypeVariable(V)
+
+    // 2. given a map with two-letter state codes as keys, and the full state names as values, write a function that
+    // prints all the states with names longer than eight characters. for example, for the mapOf("NY" to "New York",
+    // "CA" to "California"), the output would be California.
+    fun longerFullSateName(states: Map<String, String>) {
+        for (state in states) {
+            if (state.value.length > 8) {
+                println(state)
+            }
+        }
+    }
+
+    val americaStates = mapOf(
+        "NY" to "New York",
+        "CA" to "California"
+    )
+    longerFullSateName(americaStates)
+
+    // 3. write a function that combines two maps into one. if a certain key appears in both maps, ignore the pair
+    // from the first maps. this is the function's signature:
+    fun mergeMaps(
+        map1: Map<String, String>,
+        map2: Map<String, String>
+    ): Map<String, String> {
+
+        return (map1.asSequence() + map2.asSequence())
+            .distinct()
+            .groupBy({ it.key }, { it.value })
+            .mapValues { (_, values) -> values.joinToString(",") }
+    }
+
+    println(mergeMaps(map5, americaStates))
 }
