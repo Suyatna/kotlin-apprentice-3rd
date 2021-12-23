c076a7a71c8a4706d43e30a0f08e0f404e8038a4
Suyatna <zuyatna@gmail.com>
2021-12-23 15:04:45 +0700

    chapter10 - lambda as arguments

--- chapter10/src/main/kotlin/Main.kt
+++ chapter10/src/main/kotlin/Main.kt
@@ -1,7 +1,35 @@
 fun main() {
     // declaration of variable that can hold a lambda:
-    val multiplyLambda: (Int, Int) -> Int = { a: Int, b: Int -> Int
+    var multiplyLambda: (Int, Int) -> Int = { a: Int, b: Int -> Int
         a * b
     }
     println(multiplyLambda(3, 4))
+
+    // shorthand syntax
+    multiplyLambda = { a, b -> a * b}
+    println(multiplyLambda(5, 7))
+
+    // it keyword
+    val doubleLambda: (Int) -> Int = { 2 * it }
+    println(doubleLambda(10))
+
+    val square: (Int) -> Int = { it * it }
+    println(square(4))
+    println()
+
+    // lambda as arguments
+    fun operateOnNumbers(
+        a: Int,
+        b: Int,
+        operation: (Int, Int) -> Int
+    ): Int {
+        val result = operation(a, b)
+        println(result)
+        return result
+    }
+
+    val addLambda = { a: Int, b: Int ->
+        a + b
+    }
+    operateOnNumbers(4, 2, operation = addLambda)
 }
