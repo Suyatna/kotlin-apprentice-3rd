03554344d839d98b1a0979bfa9325feb9ae78282
Suyatna <zuyatna@gmail.com>
2022-01-05 18:12:53 +0700

    chapter13_challenges - complete challenges

--- chapter13_challenges/src/main/kotlin/Main.kt
+++ chapter13_challenges/src/main/kotlin/Main.kt
@@ -1,3 +1,33 @@
+import kotlin.properties.Delegates
+
+class IceCream {
+    // val name: String
+    // using default value for the name property
+    val name: String = ""
+
+    // val ingredients: ArrayList<String>
+    // lazily initialize the ingredients list
+    val ingredients: ArrayList<String> by lazy {
+        arrayListOf()
+    }
+}
+
+class FuelTank {
+    // var level = 0.0 // decimal percentage between 0 and 1
+
+    // 1. add a lowerFuel property of Boolean type to the class
+    // 2. flip the lowerFuel Boolean when the level drops below 10%
+    // 3. ensure that when the tank fills back up, the lowFuel warning will turn off
+    var lowerFuel = false
+    var level: Double by Delegates.observable(0.0) {
+        _, _, new ->
+        lowerFuel = new < 0.1
+    }
+}
+
+// 4. add a FuelTank property to Car and fill the tank. then drive around for a while
+class Car(val make: String, val color: String, val tank: FuelTank = FuelTank())
+
 fun main(args: Array<String>) {
     println("Hello World!")
 
