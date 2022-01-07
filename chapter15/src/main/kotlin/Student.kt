71461a00f5661bab2640504429acf2a2db24d6cd
Suyatna <zuyatna@gmail.com>
2022-01-07 18:50:57 +0700

    chapter15 - adding more subclassing

--- chapter15/src/main/kotlin/Student.kt
+++ chapter15/src/main/kotlin/Student.kt
@@ -1,9 +1,25 @@
-class Student(
+open class Student(
     firstName: String,
     lastName: String,
     var grades: MutableList<Grade> = mutableListOf()
-): Person(firstName, lastName) {
-    fun recordGrade(grade: Grade) {
+) : Person(firstName, lastName) {
+    open fun recordGrade(grade: Grade) {
         grades.add(grade)
     }
+}
+
+open class BandMember(
+    firstName: String,
+    lastName: String
+) : Student(firstName, lastName) {
+    open val minimumPracticeTime: Int
+        get() = 2
+}
+
+class OboePlayer(
+    firstName: String,
+    lastName: String
+) : BandMember(firstName, lastName) {
+    override val minimumPracticeTime: Int =
+        super.minimumPracticeTime * 2
 }
