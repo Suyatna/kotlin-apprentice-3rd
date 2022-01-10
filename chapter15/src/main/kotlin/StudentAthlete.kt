dac1d373e460e822d414a5e50d943b64284ab5ae
Suyatna <zuyatna@gmail.com>
2022-01-10 08:33:38 +0700

    chapter15 - when to call super

--- chapter15/src/main/kotlin/StudentAthlete.kt
+++ chapter15/src/main/kotlin/StudentAthlete.kt
@@ -2,14 +2,19 @@ class StudentAthlete(
     firstName: String,
     lastName: String
 ) : Student(firstName, lastName) {
-    private val failedClasses = mutableListOf<Grade>()
+    private var failedClasses = mutableListOf<Grade>()
 
     override fun recordGrade(grade: Grade) {
         super.recordGrade(grade)
 
-        if (grade.letter == 'F') {
-            failedClasses.add(grade)
+        var newFailedClasses = mutableListOf<Grade>()
+
+        for (grade in grades) {
+            if (grade.letter == 'F') {
+                newFailedClasses.add(grade)
+            }
         }
+        failedClasses = newFailedClasses
     }
 
     val isEligible: Boolean
