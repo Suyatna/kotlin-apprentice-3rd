50186c7d67e63336af73b63e15ae04643ff64981
Suyatna <zuyatna@gmail.com>
2022-01-12 11:35:26 +0700

    chapter16 - enum class properties and functions

--- chapter16/src/main/kotlin/DayOfTheWeek.kt
+++ chapter16/src/main/kotlin/DayOfTheWeek.kt
@@ -1,9 +1,32 @@
-enum class DayOfTheWeek {
+import java.util.*
+
+enum class DayOfTheWeek(val isWeekend: Boolean = false) {
     Monday,
     Tuesday,
     Wednesday,
     Thursday,
     Friday,
-    Saturday,
-    Sunday;
+    Saturday(true),
+    Sunday(true);
+
+    fun daysUntil(other: DayOfTheWeek): Int {
+        return if (this.ordinal < other.ordinal) {
+            other.ordinal - this.ordinal
+        } else {
+            other.ordinal - this.ordinal + values().count()
+        }
+    }
+
+    companion object {
+        fun today(): DayOfTheWeek {
+            val calendarDayOfWeek = Calendar.getInstance().get(Calendar.DAY_OF_WEEK)
+            var adjustedDay = calendarDayOfWeek - 2
+            val days = values()
+            if (adjustedDay < 0) {
+                adjustedDay += days.count()
+            }
+
+            return days.first { it.ordinal == adjustedDay }
+        }
+    }
 }
--- chapter16/src/main/kotlin/Main.kt
+++ chapter16/src/main/kotlin/Main.kt
@@ -1,6 +1,6 @@
 fun main() {
     for (day in DayOfTheWeek.values()) {
-        println("Day ${day.ordinal}: ${day.name}")
+        println("Day ${day.ordinal}: ${day.name}, is weekend: ${day.isWeekend}")
     }
     println()
 
@@ -10,4 +10,13 @@ fun main() {
 
     val tuesday = DayOfTheWeek.valueOf("Tuesday")
     println("Tuesday is day ${tuesday.ordinal}")
+    println()
+
+    val today = DayOfTheWeek.today()
+    val isWeekend = "It is ${if (today.isWeekend) "" else "not"} the weekend"
+    println("It is $today. $isWeekend")
+
+    val secondDay = DayOfTheWeek.Tuesday
+    val daysUntil = today.daysUntil(secondDay)
+    println("It is $today. $isWeekend. There are $daysUntil days until $secondDay.")
 }
