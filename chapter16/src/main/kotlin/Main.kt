a9eda3aa28899180283b63d0bbe530f725220869
Suyatna <zuyatna@gmail.com>
2022-01-12 15:30:26 +0700

    chapter16 - using when with enum classes

--- chapter16/src/main/kotlin/Main.kt
+++ chapter16/src/main/kotlin/Main.kt
@@ -19,4 +19,15 @@ fun main() {
     val secondDay = DayOfTheWeek.Tuesday
     val daysUntil = today.daysUntil(secondDay)
     println("It is $today. $isWeekend. There are $daysUntil days until $secondDay.")
+    println()
+
+    when (today) {
+        DayOfTheWeek.Monday -> println("I don't care if $today's blue")
+        DayOfTheWeek.Tuesday -> println("$today's gray")
+        DayOfTheWeek.Wednesday -> println("And $today, too")
+        DayOfTheWeek.Thursday -> println("$today, I don't care 'bout you")
+        DayOfTheWeek.Friday -> println("It's $today, I'm in love")
+        DayOfTheWeek.Saturday -> println("$today, wait...")
+        DayOfTheWeek.Sunday -> println("$today always comes too late")
+    }
 }
