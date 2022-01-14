d390e777fa133e1c3d6bdc769626fe64e7e8ff39
Suyatna <zuyatna@gmail.com>
2022-01-14 15:32:12 +0700

    chapter18 - anatomy of standard library generic types

--- chapter18/src/main/kotlin/Main.kt
+++ chapter18/src/main/kotlin/Main.kt
@@ -1,6 +1,26 @@
+import kotlin.time.measureTimedValue
+
 fun main() {
-    val names: List<String> = listOf("Bob", "Carol", "Ted", "Alice")
+    val names = listOf("Bob", "Carol", "Ted", "Alice")
     println("Names: $names")
     val firstName = names.first()
     println(firstName)
+    println()
+
+    val things= mutableListOf<Any>(1, 2)
+    things.add("Steve")
+    println("Things: $things")
+    println()
+
+    val map = mapOf(
+        Pair("one", 1),
+        Pair("two", "II"),
+        Pair("three", 3.0f)
+    )
+
+    val valuesForKeysWithE = map.keys
+        .filter { it.contains("e") }
+        .map { "Value for $it: ${map[it]}" }
+    println("Values for keys with E: $valuesForKeysWithE")
+    println()
 }
