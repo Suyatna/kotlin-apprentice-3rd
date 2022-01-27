f53a7c602b4da45557bc663902f33464596274af
Suyatna <zuyatna@gmail.com>
2022-01-27 10:51:03 +0700

    chapter19 - java getter setters class to kotlin

--- chapter19/src/main/kotlin/Main.kt
+++ chapter19/src/main/kotlin/Main.kt
@@ -1,7 +1,11 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
+fun main() {
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+    val user = User()
+
+    user.firstName = "Bob"
+    user.lastName = "Parker"
+    user.city = "Los Angeles"
+    user.country = "United States"
+
+    println("User info:\n$user")
 }
