5703414959bb96eb2926835aa317daab428a7be1
Suyatna <zuyatna@gmail.com>
2022-02-03 15:36:58 +0700

    chapter20 - proper way to throw exceptions

--- chapter20/src/main/kotlin/SpaceCraft.kt
+++ chapter20/src/main/kotlin/SpaceCraft.kt
@@ -6,19 +6,23 @@ class SpaceCraft {
 
     fun launch() {
         if (fuel < 5) {
-            sendMessageToEarth("Out of fuel. Can't take off")
+//            sendMessageToEarth("Out of fuel. Can't take off")
+            throw Exception("Out of fuel. Can't take off")
             return
         }
 
         if (!isEngineInOrder) {
-            sendMessageToEarth("The engine is broken. Can't take off")
+//            sendMessageToEarth("The engine is broken. Can't take off")
+            throw Exception("The engine is broken. Can't take off")
             return
         }
 
         if (!isConnectionAvailable) {
-            sendMessageToEarth("No connection with Earth. Can't take off")
+//            sendMessageToEarth("No connection with Earth. Can't take off")
+            throw Exception("No connection with Earth. Can't take off")
             return
         }
+        // remaining launch code
 
         sendMessageToEarth("Trying to launch...")
         fuel -= 5
