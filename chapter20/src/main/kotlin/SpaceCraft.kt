f12c42fc1da4ab7b28feb472dc3feea67faf924e
Suyatna <zuyatna@gmail.com>
2022-02-03 15:32:11 +0700

    chapter20 - throwing exceptions

---
+++ chapter20/src/main/kotlin/SpaceCraft.kt
@@ -0,0 +1,39 @@
+class SpaceCraft {
+    private var isConnectionAvailable: Boolean = false
+    private var isEngineInOrder: Boolean = false
+    private var fuel: Int = 0
+    var isInSpace: Boolean = false
+
+    fun launch() {
+        if (fuel < 5) {
+            sendMessageToEarth("Out of fuel. Can't take off")
+            return
+        }
+
+        if (!isEngineInOrder) {
+            sendMessageToEarth("The engine is broken. Can't take off")
+            return
+        }
+
+        if (!isConnectionAvailable) {
+            sendMessageToEarth("No connection with Earth. Can't take off")
+            return
+        }
+
+        sendMessageToEarth("Trying to launch...")
+        fuel -= 5
+        sendMessageToEarth("I'm in space!")
+        sendMessageToEarth("I've found some extraterrestrials")
+        isInSpace = true
+    }
+
+    private fun sendMessageToEarth(message: String) {
+        println("Spacecraft to Earth: $message")
+    }
+}
+
+object SpacePort {
+    fun investigateSpace(spaceCraft: SpaceCraft) {
+        spaceCraft.launch()
+    }
+}
