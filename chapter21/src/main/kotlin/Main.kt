10519d4f5ed994b1b77867810fcf2b84dd5d8540
Suyatna <zuyatna@gmail.com>
2022-02-04 23:09:51 +0700

    chapter21 - getting start functional programming

--- chapter21/src/main/kotlin/Main.kt
+++ chapter21/src/main/kotlin/Main.kt
@@ -1,7 +1,6 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
+fun main() {
+    val firstRobot = Robot("Experimental Space Navigation Droid")
+    val secondRobot = Robot("Extra-Terrestrial Air Safety Droid")
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+    Battlefield.beginBattle(firstRobot, secondRobot)
 }
---
+++ chapter21/src/main/kotlin/Robot.kt
@@ -0,0 +1,38 @@
+import java.util.*
+
+class Robot(val name: String) {
+    var strength: Int = 0
+    var isAlive: Boolean = true
+    private var health: Int = 100
+
+    init {
+        strength = Random().nextInt(100) + 10
+        report("Create (strength: $strength)")
+    }
+
+    private fun report(message: String) {
+        println("name:\t$message")
+    }
+
+    private fun damage(damage: Int) {
+        val blocked = Random().nextBoolean()
+
+        if (blocked) {
+            report("Blocked attack")
+            return
+        }
+
+        health -= damage
+        report("Damage -$damage, health $health")
+
+        if (health <= 0) {
+            isAlive = false
+        }
+    }
+
+    fun attack(robot: Robot) {
+        val damage = (strength * 0.1 + Random().nextInt(10)).toInt()
+
+        robot.damage(damage)
+    }
+}
