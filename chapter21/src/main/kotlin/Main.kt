3b283e4727162673d6b52ddd074008f986c23598
Suyatna <zuyatna@gmail.com>
2022-02-05 10:42:50 +0700

    chapter21 - lambdas

--- chapter21/src/main/kotlin/Main.kt
+++ chapter21/src/main/kotlin/Main.kt
@@ -2,9 +2,13 @@ fun main() {
     val firstRobot = Robot("Experimental Space Navigation Droid")
     val secondRobot = Robot("Extra-Terrestrial Air Safety Droid")
 
-    Battlefield.beginBattle(firstRobot, secondRobot, ::onBattleEnded)
+//    Battlefield.beginBattle(firstRobot, secondRobot, ::onBattleEnded)
+
+    Battlefield.beginBattle(firstRobot, secondRobot) {
+        it.report("Win!")
+    }
 }
 
-fun onBattleEnded(winner: Robot) {
-    winner.report("Win!")
-}
+//fun onBattleEnded(winner: Robot) {
+//    winner.report("Win!")
+//}
