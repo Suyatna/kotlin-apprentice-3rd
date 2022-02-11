7b5c426ac4e4941dbb18c4458aa64b894bea57b6
Suyatna <zuyatna@gmail.com>
2022-02-11 14:24:27 +0700

    chapter21_challenges - challenge 1 create Battlefield.kt class

---
+++ chapter21_challenges/src/main/kotlin/Battlefield.kt
@@ -0,0 +1,32 @@
+object Battlefield {
+
+    inline fun beginBattle(
+        firstRobot: Robot,
+        secondRobot: Robot,
+        onBattleEnded: (Robot) -> Unit
+    ) {
+        var winner: Robot? = null
+
+        battle(firstRobot, secondRobot)
+
+        winner = if (firstRobot.isAlive) firstRobot else secondRobot
+
+        onBattleEnded(winner)
+    }
+
+    tailrec fun battle(firstRobot: Robot, secondRobot: Robot) {
+        firstRobot.attack(secondRobot)
+
+        if (secondRobot.isAlive.not()) {
+            return
+        }
+
+        secondRobot.attack(firstRobot)
+
+        if (firstRobot.isAlive.not()) {
+            return
+        }
+
+        battle(firstRobot, secondRobot)
+    }
+}
