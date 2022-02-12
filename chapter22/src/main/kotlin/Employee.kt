05faeccf604ce0bc92b4073bfe104e6b17d447d3
Suyatna <zuyatna@gmail.com>
2022-02-12 10:17:55 +0700

    chapter22 - unary operator overloading

--- chapter22/src/main/kotlin/Employee.kt
+++ chapter22/src/main/kotlin/Employee.kt
@@ -3,4 +3,15 @@ class Employee(
     val name: String,
     var salary: Int
 ) {
+    operator fun inc(): Employee {
+        salary += 5000
+        println("$name got a raise to $$salary")
+        return this
+    }
+
+    operator fun dec(): Employee {
+        salary -= 5000
+        println("$name's salary decreased to $$salary")
+        return this
+    }
 }
