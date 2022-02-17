19e966251c044c3c6134ece38f2153fe471fdbec
Suyatna <zuyatna@gmail.com>
2022-02-17 19:21:32 +0700

    chapter23 - threads

--- chapter23/src/main/kotlin/Main.kt
+++ chapter23/src/main/kotlin/Main.kt
@@ -1,7 +1,13 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
+import kotlin.concurrent.thread
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+fun main() {
+    thread(start = true, name = "another thread") {
+        (0..10).forEach {
+            println("Message #$it from ${Thread.currentThread().name}")
+        }
+    }
+
+    (0..10).forEach {
+        println("Message #$it from ${Thread.currentThread().name}")
+    }
 }
