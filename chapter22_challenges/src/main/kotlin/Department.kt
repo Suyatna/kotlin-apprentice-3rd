ba8baaadbc60a5403e9207bd9c6e4c77623b8acb
Suyatna <zuyatna@gmail.com>
2022-02-17 13:18:43 +0700

    chapter22_challenges - initialize Department.kt

--- chapter22_challenges/src/main/kotlin/Department.kt
+++ chapter22_challenges/src/main/kotlin/Department.kt
@@ -2,7 +2,37 @@ class Department(val name: String) : Iterable<Employee> {
 
     val employees: ArrayList<Employee> = arrayListOf()
 
-    override fun iterator(): Iterator<Employee> {
-        TODO("Not yet implemented")
+    operator fun plusAssign(employee: Employee) {
+        employees.add(employee)
+        println("${employee.name} hired to $name department")
     }
+
+    operator fun minusAssign(employee: Employee) {
+        if (employees.contains(employee)) {
+            employees.remove(employee)
+            println("${employee.name} fired from $name department")
+        }
+    }
+
+    fun add(newEmployees: List<Employee>) {
+        employees.addAll(newEmployees)
+    }
+
+    operator fun get(index: Int): Employee? {
+        return if (index < employees.size) {
+            employees[index]
+        } else {
+            null
+        }
+    }
+
+    operator fun set(index: Int, employee: Employee) {
+        if (index < employees.size) {
+            employees[index] = employee
+        }
+    }
+
+    operator fun contains(employee: Employee) = employees.contains(employee)
+
+    override fun iterator(): Iterator<Employee> = employees.iterator()
 }
