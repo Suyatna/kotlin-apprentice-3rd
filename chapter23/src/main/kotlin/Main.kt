3e34bdf55443039b3735e7dcdf22b6d11b254205
Suyatna <zuyatna@gmail.com>
2022-02-18 11:40:40 +0700

    chapter23 - initialize Building.kt

--- chapter23/src/main/kotlin/Main.kt
+++ chapter23/src/main/kotlin/Main.kt
@@ -1,13 +1,3 @@
-import kotlin.concurrent.thread
-
 fun main() {
-    thread(start = true, name = "another thread") {
-        (0..10).forEach {
-            println("Message #$it from ${Thread.currentThread().name}")
-        }
-    }
 
-    (0..10).forEach {
-        println("Message #$it from ${Thread.currentThread().name}")
-    }
 }
